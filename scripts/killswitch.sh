222bcd88d8b997605319ac9a77f4fea9b53f5d88
supersunho <supersunho@gmail.com>
2025-06-15 22:55:04 +0900

    update killswitch.sh

--- scripts/killswitch.sh
+++ scripts/killswitch.sh
@@ -2,7 +2,7 @@
 
 export ADGUARD_USE_KILL_SWITCH_CHECK_INTERVAL=${ADGUARD_USE_KILL_SWITCH_CHECK_INTERVAL:-30}
 
-INITIAL_IP=$(curl -s https://ifconfig.me/ip)
+INITIAL_IP=$(curl -4 ifconfig.me/ip)
 echo " > [Kill Switch] Initial IP: $INITIAL_IP" 
 
 VPN_IP=""
@@ -15,7 +15,7 @@ while true; do
         sleep $ADGUARD_USE_KILL_SWITCH_CHECK_INTERVAL
     fi
  
-    CURRENT_IP=$(curl -s https://ifconfig.me/ip)
+    CURRENT_IP=$(curl -4 ifconfig.me/ip)
     echo " > [Kill Switch] Initial IP: $INITIAL_IP => Current IP: $CURRENT_IP" 
 
     if [ "$VPN_CONNECTED" -eq 0 ]; then
